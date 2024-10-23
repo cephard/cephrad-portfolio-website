85f55e5cdea79308919a38ece6471b37a1ab726f
CEPHARD KIPKEMBOI <58904994+cephard@users.noreply.github.com>
2024-10-23 14:15:36 +0100

    first comit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showInfo(index) {
+    // Hide all sections
+    let sections = document.querySelectorAll('.info-section');
+    sections.forEach(section => section.style.display = 'none');
+
+    // Show the selected section
+    document.getElementById('info-' + index).style.display = 'flex';
+}
