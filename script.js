143aca897d97dbdbd3c5762daa35b9e0d50176e1
CEPHARD KIPKEMBOI <58904994+cephard@users.noreply.github.com>
2024-12-07 21:42:35 +0000

    contact added

--- script.js
+++ script.js
@@ -96,4 +96,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      window.scrollTo({
+          top: targetElement.offsetTop - document.querySelector('.header-section').offsetHeight,
+          behavior: 'smooth'
+      });
+  });
+});
 
